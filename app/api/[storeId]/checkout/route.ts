117ec141167fe4e791b073e1d648148b21bd7aa0
Dhimm <66907668+f4kesmile@users.noreply.github.com>
2025-06-19 21:23:49 +0700

    Update checkout route.ts
    cors headers

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -3,7 +3,7 @@ import midtransClient from "midtrans-client";
 import db from "@/lib/db";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "http://localhost:3001", // Ganti dengan domain frontend Anda
+  "Access-Control-Allow-Origin": "https://ecommerce-frontend-henna-one.vercel.app", // Ganti dengan domain frontend Anda
   "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
